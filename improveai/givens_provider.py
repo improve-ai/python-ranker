8a439b1392e26b648e71144faa826ce78d709e1c
Konrad <konrad.wiecko@gmail.com>
2022-03-17 20:01:05 +0100

    added remarks about GivensProvider implementation

--- improveai/givens_provider.py
+++ improveai/givens_provider.py
@@ -1,6 +1,14 @@
+import improveai.decision_model as dm
+
+
 class GivensProvider:
 
     # TODO how givens should be provided - should it be some sort of customized
     #  implementation for each model dynamically providing givens?
-    def givens(self, for_model, givens: dict = {}) -> dict:
+    #  also in iOS this method returns @{} and is named givensForModel
+    #  (but maybe I got this wrong)
+    #  I made it return provided givens -> which implementation is correct
+    def givens(self, for_model, givens: dict or None = {}) -> dict:
+        assert isinstance(for_model, dm.DecisionModel)
+        assert isinstance(givens, dict) or givens is None
         return givens
