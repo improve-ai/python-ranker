6963327ea9157fe4984bd72404f2568492852fc6
Konrad <konrad.wiecko@gmail.com>
2022-03-23 16:11:52 +0100

    Made DecisionContext and GivensProvider available in __init__.py to allow for shorter imports

--- improveai/__init__.py
+++ improveai/__init__.py
@@ -1,8 +1,10 @@
 from improveai.cythonized_feature_encoding import cfe
 from improveai.decision import Decision
+from improveai.decision_context import DecisionContext
 from improveai.feature_encoder import FeatureEncoder
 from improveai.decision_model import DecisionModel
 from improveai.decision_tracker import DecisionTracker
+from improveai.givens_provider import GivensProvider
 from improveai.settings import USE_CYTHON_BACKEND
 
 FastFeatureEncoder = cfe.FeatureEncoder
