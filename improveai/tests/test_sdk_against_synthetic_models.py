ea39d5b109381110d7ea060d39c5560ea9abe775
KWiecko <konrad.wiecko@gmail.com>
2023-03-17 16:50:05 +0100

    More updates of synthetic models tests

--- improveai/tests/test_sdk_against_synthetic_models.py
+++ improveai/tests/test_sdk_against_synthetic_models.py
@@ -5,7 +5,7 @@ import numpy as np
 import requests_mock as rqm
 from tqdm import tqdm
 
-from improveai import DecisionModel
+from improveai import Scorer
 
 
 MODELS_DIR = os.getenv('DECISION_MODEL_PREDICTORS_DIR')
@@ -26,78 +26,71 @@ def test_sdk_against_all_synthetic_models():
         with open(test_case_json_path, 'r') as tcf:
             test_case = json.loads(tcf.read())
 
-        # load model
-        dm = DecisionModel(model_name=None, track_url=TRACK_URL)\
-            .load(os.sep.join([SDK_PATH, test_case['model_url']]))
+        # load scorer
+        scorer = Scorer(model_url=os.sep.join([SDK_PATH, test_case['model_url']]))
 
-        # dt = DecisionTracker(track_url=TRACK_URL)
-        # dm.track_with(tracker=dt)
-
-        all_givens = test_case['test_case']['givens']
-        variants = test_case['test_case']['variants']
+        all_contexts = test_case['test_case']['contexts']
+        candidates = test_case['test_case']['candidates']
         noise = test_case['test_case']['noise']
         seed = test_case['test_case']['python_seed']
         expected_outputs = test_case['expected_output']
 
-        # for each givens
-        if all_givens is None:
-            all_givens = [None]
+        # for each context
+        if all_contexts is None:
+            all_contexts = [None]
 
-        for givens, output in zip(all_givens, expected_outputs):
-            with rqm.Mocker() as m:
-                m.post(TRACK_URL, text='success')
-                dm.chooser.imposed_noise = noise
-                scores = dm._score(variants=variants, givens=givens)
-                chosen_variant = variants[np.argmax(scores)]
+        for context, output in zip(all_contexts, expected_outputs):
+            scorer.chooser.imposed_noise = noise
+            np.random.seed(seed)
+            scores = scorer.score(items=candidates, context=context)
+            chosen_item = candidates[np.argmax(scores)]
 
-                assert chosen_variant == output['variant']
-                assert all(abs(scores - output['scores']) < 2 ** -22)
+            assert chosen_item == output['item']
+            assert all(abs(scores - output['scores']) < 2 ** -22)
 
 
-def test_primitive_predicts_identical_with_primitive_dicts():
-    test_case_dirs = \
-        ['1000_numeric_variants_20_same_nested_givens_large_binary_reward',
-         'primitive_variants_no_givens_binary_reward']
-    for test_case_dir in test_case_dirs:
-        test_case_json_path = \
-            os.sep.join([SYNTHETIC_MODELS_DIR, test_case_dir, '{}.json'.format(test_case_dir)])
-        with open(test_case_json_path, 'r') as tcf:
-            test_case = json.loads(tcf.read())
-
-        # load model
-        dm = DecisionModel(model_name=None, track_url=TRACK_URL)\
-            .load(os.sep.join([SDK_PATH, test_case['model_url']]))
-
-        all_givens = test_case['test_case']['givens']
-
-        if all_givens is None:
-            all_givens = [None]
-
-        variants = test_case['test_case']['variants']
-        dicts_variants = [{"$value": v} for v in test_case['test_case']['variants']]
-        noise = test_case['test_case']['noise']
-        seed = test_case['test_case']['python_seed']
-        expected_outputs = test_case['expected_output']
-
-        for givens, output in zip(all_givens, expected_outputs):
-            with rqm.Mocker() as m:
-                m.post(TRACK_URL, text='success')
-                dm.chooser.imposed_noise = noise
-                scores = dm._score(variants=variants, givens=givens)
-                chosen_variant = variants[np.argmax(scores)]
-
-                assert chosen_variant == output['variant']
-                assert all(abs(scores - output['scores']) < 2 ** -22)
-
-        for givens, output in zip(all_givens, expected_outputs):
-            with rqm.Mocker() as m:
-                m.post(TRACK_URL, text='success')
-                dm.chooser.imposed_noise = noise
-                scores = dm._score(variants=dicts_variants, givens=givens)
-                chosen_variant = dicts_variants[np.argmax(scores)]
-
-                assert chosen_variant == {"$value": output['variant']}
-                assert all(abs(scores - output['scores']) < 2 ** -22)
+# TODO this test no longer makes sense for a single model (due to feature names)
+# def test_primitive_predicts_identical_with_primitive_dicts():
+#     test_case_dirs = \
+#         ['1000_numeric_variants_20_same_nested_givens_large_binary_reward',
+#          'primitive_variants_no_givens_binary_reward']
+#     for test_case_dir in test_case_dirs:
+#         test_case_json_path = \
+#             os.sep.join([SYNTHETIC_MODELS_DIR, test_case_dir, '{}.json'.format(test_case_dir)])
+#         with open(test_case_json_path, 'r') as tcf:
+#             test_case = json.loads(tcf.read())
+#
+#         # load model
+#         scorer = Scorer(model_url=os.sep.join([SDK_PATH, test_case['model_url']]))
+#
+#         all_contexts = test_case['test_case']['contexts']
+#
+#         if all_contexts is None:
+#             all_contexts = [None]
+#
+#         candidates = test_case['test_case']['candidates']
+#         dicts_variants = [{"$value": v} for v in test_case['test_case']['candidates']]
+#         noise = test_case['test_case']['noise']
+#         seed = test_case['test_case']['python_seed']
+#         expected_outputs = test_case['expected_output']
+#
+#         for context, output in zip(all_contexts, expected_outputs):
+#
+#             scorer.chooser.imposed_noise = noise
+#             scores = scorer.score(items=candidates, context=context)
+#             chosen_variant = candidates[np.argmax(scores)]
+#
+#             assert chosen_variant == output['item']
+#             assert all(abs(scores - output['scores']) < 2 ** -22)
+#
+#         for context, output in zip(all_contexts, expected_outputs):
+#
+#             scorer.chooser.imposed_noise = noise
+#             scores = scorer.score(items=dicts_variants, context=context)
+#             chosen_variant = dicts_variants[np.argmax(scores)]
+#
+#             assert chosen_variant == {"$value": output['item']}
+#             assert all(abs(scores - output['scores']) < 2 ** -22)
 
 
 def test_model_predicts_identical_for_nullish_variants():
@@ -109,23 +102,19 @@ def test_model_predicts_identical_for_nullish_variants():
     with open(test_case_json_path, 'r') as tcf:
         test_case = json.loads(tcf.read())
 
-    # load model
-    dm = \
-        DecisionModel(model_name=None, track_url=TRACK_URL)\
-        .load(os.sep.join([SDK_PATH, test_case['model_url']]))
+    scorer = Scorer(os.sep.join([SDK_PATH, test_case['model_url']]))
 
-    variants = [None, {}, [], {'$value': None}]
+    candidates = [None, {}, [], {'$value': None}]
     noise = 0.1
     seed = 0
 
-    with rqm.Mocker() as m:
-        m.post(TRACK_URL, text='success')
-        dm.chooser.imposed_noise = noise
-        scores = dm._score(variants=variants, givens=None)
+    scorer.chooser.imposed_noise = noise
+    np.random.seed(seed)
+    scores = scorer.score(items=candidates, context=None)
 
-        for bsc_index, bsc in enumerate(scores):
-            for osc_index, osc in enumerate(scores):
-                if bsc_index == osc_index:
-                    continue
-                assert abs(bsc - osc) < 2 ** -22
+    for bsc_index, bsc in enumerate(scores):
+        for osc_index, osc in enumerate(scores):
+            if bsc_index == osc_index:
+                continue
+            assert abs(bsc - osc) < 2 ** -22
 
