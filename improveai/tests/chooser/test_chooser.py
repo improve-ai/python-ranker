5249ee6c7d8829c62b6c6b9f6f296d2b7e7eaa1e
KWiecko <konrad.wiecko@gmail.com>
2023-03-19 15:46:22 +0100

    updated chooser tests

--- improveai/tests/chooser/test_chooser.py
+++ improveai/tests/chooser/test_chooser.py
@@ -70,16 +70,6 @@ def test_bad_model_name():
     for bad_model_name in bad_model_names:
         with raises(AssertionError) as aerr:
             chooser.model_name = bad_model_name
-            # assert aerr.value
-
-# TODO test IO errors
-#         model_metadata = self._get_model_metadata()
-#         self.model_seed = self._get_model_seed(model_metadata=model_metadata)
-#         self.model_name = self._get_model_name(model_metadata=model_metadata)
-#         self.feature_names = self._get_model_feature_names(model_metadata=model_metadata)
-#         self.string_tables = self._get_string_tables(model_metadata=model_metadata)
-#         self.improveai_major_version_from_metadata = \
-#             self._get_improveai_major_version(model_metadata=model_metadata)
 
 
 def test__get_model_metadata_raises_for_no_user_metadata_attr():
@@ -403,12 +393,6 @@ def test__get_improveai_major_version_for_none_metadata():
     global chooser
     with raises(IOError) as ioe:
         chooser._get_improveai_major_version(model_metadata=None)
-
-
-# def test__get_improveai_major_version_for_empty_metadata():
-#     global chooser
-#     with raises(IOError) as ioe:
-#         chooser._get_improveai_major_version(model_metadata={})
 
 
 def test__get_improveai_major_version_for_none_major_improveai_version():
